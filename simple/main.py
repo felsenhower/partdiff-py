92a70c3bcbe0eb4f29405c0989760a0b56c39e6b
Ruben Felgenhauer <Ruben.Felgenhauer@posteo.de>
2025-12-27 13:17:28 +0100

    Rename matrix variables

--- simple/main.py
+++ simple/main.py
@@ -25,10 +25,10 @@ def calculate(arguments: CalculationArguments, options: Options) -> CalculationR
     perturbation_matrix = arguments.perturbation_matrix
     stat_iteration = 0
     stat_accuracy = None
-    output_matrix = tensor[0, :, :]
-    input_matrix = output_matrix
+    matrix_out = tensor[0, :, :]
+    matrix_in = matrix_out
     if options.method == CalculationMethod.JACOBI:
-        input_matrix = tensor[1, :, :]
+        matrix_in = tensor[1, :, :]
     finished = False
     while not finished:
         stat_iteration += 1
@@ -38,24 +38,24 @@ def calculate(arguments: CalculationArguments, options: Options) -> CalculationR
         for i in range(1, n):
             for j in range(1, n):
                 star = 0.25 * (
-                    input_matrix[i - 1, j]
-                    + input_matrix[i, j - 1]
-                    + input_matrix[i, j + 1]
-                    + input_matrix[i + 1, j]
+                    matrix_in[i - 1, j]
+                    + matrix_in[i, j - 1]
+                    + matrix_in[i, j + 1]
+                    + matrix_in[i + 1, j]
                 )
                 star += perturbation_matrix[i, j]
                 if options.termination == TerminationCondition.ACCURACY or finished:
-                    residuum = abs(input_matrix[i, j] - star)
+                    residuum = abs(matrix_in[i, j] - star)
                     maxresiduum = max(maxresiduum, residuum)
-                output_matrix[i, j] = star
+                matrix_out[i, j] = star
         stat_accuracy = maxresiduum
-        input_matrix, output_matrix = output_matrix, input_matrix
+        matrix_in, matrix_out = matrix_out, matrix_in
         if options.termination == TerminationCondition.ACCURACY:
             if maxresiduum < options.term_accuracy:
                 finished = True
     end_time = time()
     duration = end_time - start_time
-    final_matrix = input_matrix
+    final_matrix = matrix_in
     return CalculationResults(final_matrix, stat_iteration, stat_accuracy, duration)
 
 
